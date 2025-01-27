9c1637c42b6af2e820667865c9ed051cce95252c
doaasafwat <doaasafwat54@gmail>
2025-01-27 21:56:00 +0200

    category item done

--- lib/views/Categories/category_item_list_view.dart
+++ lib/views/Categories/category_item_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:online_courses_app/views/Categories/category_item.dart';
+import 'package:online_courses_app/views/ShowAll/show_all_view.dart';
 
 class CategoryItemListView extends StatelessWidget {
   const CategoryItemListView({super.key});
@@ -12,9 +13,18 @@ class CategoryItemListView extends StatelessWidget {
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return const CategoryItem(
-            image: 'assets/image/categoryImage.png',
-            title: 'Programming\nSoftware',
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const ShowAllView(
+                  title: 'Programming Software',
+                );
+              }));
+            },
+            child: const CategoryItem(
+              image: 'assets/image/categoryImage.png',
+              title: 'Programming\nSoftware',
+            ),
           );
         },
       ),
